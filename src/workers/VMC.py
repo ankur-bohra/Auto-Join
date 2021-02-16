7718d1d7cc76d83854fc4ef92c449ee7c42acece
ankurbohra04 <ankurbohra04@gmail.com>
2021-02-16 23:02:38 +0530

    Get to target group, switch to firefox

--- src/workers/VMC.py
+++ src/workers/VMC.py
@@ -1,29 +1,26 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from multicondition import all_of, none_of
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
-print(EC.presence_of_element_located)
-print(all_of)
-
-WHATSAPP_URL  = 'http://web.whatsapp.com'
-PROFILE_PATH = r'C:\Users\Admin\AppData\Local\Google\Chrome\User Data\Profile 2'
-
-#Popen(['chrome', '--remote-debugging-port=2131'])
+WHATSAPP = 'http://web.whatsapp.com'
+PROFILE_PATH = 'C:/Users/pbohr/AppData/Roaming/Mozilla/Firefox/Profiles/rvqzov3w.AutoMate'
 
 # WhatsApp needs to be logged in
-options = webdriver.ChromeOptions()
-options.add_argument('--start-maximized')
-options.add_argument('--user-data-dir=AutoMate User Data')
-options.add_argument('--profile-directory=Profile 2')
+options = Options()
+options.profile = PROFILE_PATH
 
-browser = webdriver.Chrome(options=options)
-browser.get('https://web.whatsapp.com/')
+service = Service('geckodriver')
+
+driver = webdriver.Firefox(options=options, service=service)
+driver.get(WHATSAPP)
 
 # Wait for whatsapp to load in our initiating element
-WebDriverWait(browser, 10).until(
+WebDriverWait(driver, 10).until(
     EC.all_of(
         EC.presence_of_element_located((By.CSS_SELECTOR, "span[title='VMC OIC-2 batch 2022'")),
         EC.none_of(
@@ -33,13 +30,20 @@ WebDriverWait(browser, 10).until(
     )
 )
 
-source = browser.page_source
-#soup = BeautifulSoup(source, 'html.parser')
-group_name_element = browser.find_element_by_css_selector("span[title='VMC OIC-2 batch 2022'")
-print(group_name_element)
-# Figure out how to get to actual group div from the title='VMC OIC-2 batch 2022' span
-# Figure out where to click
-# Can we click?
+# Get to the actual gruop
+group_name_span = driver.find_element(By.CSS_SELECTOR,"span[title='VMC OIC-2 batch 2022'")
+location = group_name_span.location
+action = ActionChains(driver)
+action.move_to_element(group_name_span).click().perform()
+
+# Get all the messages
+messages = driver.find_elements((By.CLASS_NAME, 'message-in'))
+
+# TODO:
+# Use XPaths to get to the class and id-less <span> tag with the main text
+# Feed it into a regex and pick off the link
+# If a valid link is found get the time
+# Sort by time 
 
 input()
-browser.quit()
+driver.quit()
