9d4be060ba46642d29d59ff79bd6a3c712309539
ankurbohra04 <ankurbohra04@gmail.com>
2021-03-09 16:45:27 +0530

    Make central job executor

--- src/workers/__init__.py
+++ src/workers/__init__.py
@@ -1,1 +1,10 @@
-from . import HXLS
+from . import HXLS, VMC
+
+worker_area_registry = {
+    "HXLS": HXLS,
+    "VMC": VMC
+}
+
+def execute_job(area):
+    worker = worker_area_registry[area]
+    worker.primary_action()
