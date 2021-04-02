32130894a881ab93ad0452b9af7ab8246374bbf7
ankurbohra04 <ankurbohra04@gmail.com>
2021-04-02 11:18:28 +0530

    refactor!: start from scratch

--- src/sources/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import google_calendar#, whatsapp
-
-source_codes = {
-    "CLNDR": google_calendar,
-    # "WHTSP": whatsapp
-}
-
-def get_source(source):
-    return source_codes[source]
--- src/sources/util/__init__.py
+++
@@ -1,2 +0,0 @@
-from .time import now, offset
-from .zoom import join_from_link
