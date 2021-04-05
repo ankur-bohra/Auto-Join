221e00144dd86694a276df681d4709d1de996a61
ankurbohra04 <ankurbohra04@gmail.com>
2021-04-05 22:09:07 +0530

    fix: reverse accidental revert
    Accidently discarded changes to calendar_api during initial commit

--- src/calendar_api.py
+++ src/calendar_api.py
@@ -1,8 +1,9 @@
 import os
-from typing import Sequence, TextIO, Union, Optional
+from typing import Sequence, TextIO, Union, Optional, List, Dict, Type, Tuple, Int
 
 from _io import TextIOWrapper # type: ignore
-from google.auth.credentials import Credentials
+from datetime import datetime
+from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build, Resource
@@ -12,7 +13,7 @@ scopes = [
     "https://www.googleapis.com/auth/calendar.events.readonly"
 ]
 
-def get_creds(scopes: Sequence[str], data_folder: Union[str, TextIO] = "data", show_auth_prompt: bool = True) -> Credentials:
+def get_creds(scopes: Sequence[str], data_folder: Union[str, TextIO] = "data", show_auth_prompt: bool = True) -> Type[Credentials]:
     """Get/create user credentials in given folder with specified scopes.
 
     Args:
@@ -26,13 +27,14 @@ def get_creds(scopes: Sequence[str], data_folder: Union[str, TextIO] = "data", s
     # Convert data_folder to string if not already so
     if type(data_folder) == TextIOWrapper:
         data_folder = data_folder.name
-
-    creds: Optional[Credentials] = None
+    
+    creds: Optional[Type[Credentials]] = None
     # The file token.json stores the user"s access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists(data_folder+"\\token.json"):
         creds = Credentials.from_authorized_user_file(data_folder+"\\token.json", scopes)
+        
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -63,10 +65,54 @@ def get_service() -> Resource:
         service = build("calendar", "v3", credentials=creds)
     return service
 
-def get_calendar_list():
-    service: Resource = get_service()
+def get_calendar_list() -> Resource:
+    '''Get a calendar list's items with the first 100 calendars.
+
+    Returns:
+        A list of calendarListEntrys.
+    '''
+    service = get_service()
     calendar_list = service.calendarList().list().execute() # Default maxResults = 100 which is sufficient
     return calendar_list.get("items")
 
+def get_calendar_from_list_entry(calendar_list_entry: dict) -> str:
+    '''Get the calendar from a calendarListEntry.
+
+    Args:
+        calendar_list_entry: A calendarListEntry dictionary from calendarList.items
+
+    Returns:
+        The calendar object associated with the given calendarListEntry's id.
+    '''
+    service = get_service()
+    # calendarListEntry has partial, useless data and isn't
+    # very useful in the API, directly getting calendar is 
+    # more sensible
+    calendar_id = calendar_list_entry.get("id") 
+    calendar = service.calendars().get(calendarId=calendar_id).execute() 
+    return calendar
+
+def get_events_in_time_span(calendar: Dict, time_from: Union[datetime, str], time_to: Union[datetime, str], allow_incomplete_overlaps: bool = False) -> List[Dict]:
+    '''Get events inside a time span from the given calendar.
+
+    Args:
+        calendar: A Calendar dictionary.
+        time_from: An RFC3999 string or datetime object denoting the start of the time span (inclusve).
+        time_to: An RFC3999 string or datetime object denoting the end of the time span (exclusive).
+        allow_incomplete_overlaps: Whether to include events not completely inside the time span. Defaults to false. 
+
+    Returns:
+        A list of Events each with an added field "overlapType" of possible values:
+            "Complete": The Event starts and ends inside the time span.
+            "Partial": The Event only either starts or ends inside the time span.
+    '''
+    service = get_service()
+    # TODO: Write function body
+    #  Use https://developers.google.com/calendar/v3/reference/events/list
+
+
 if __name__ == "__main__":
-    print(get_calendar_list())
+    result = get_calendar_list()
+    teamie_calendar_list_entry = result[2]
+    teamie_calendar = get_calendar_from_list_entry(teamie_calendar_list_entry)
+    print(teamie_calendar)
